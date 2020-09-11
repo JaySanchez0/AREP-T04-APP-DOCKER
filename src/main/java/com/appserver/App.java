32bd9c3a43518dd94e5347c24747c036df783ed4
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-09-11 18:14:26 -0500

    First commit

---
+++ src/main/java/com/appserver/App.java
@@ -0,0 +1,13 @@
+package com.appserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
